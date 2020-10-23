2148f7f0f8e5f70dbf994109ec5b8691d055c123
yuyuan871111 <adp871111@gmail.com>
2020-10-23 18:20:44 +0800

    fastp file debugging

--- autoscript/fastp.sh
+++ autoscript/fastp.sh
@@ -6,17 +6,17 @@
 #PBS -o fastp.std
 #PBS -e fastp.err
 
-if [ -f "$fileroad/$filename.read1.fastq" ];
+if [ -f "$var1/${var2}.read1.fastq" ];
 then
-	/pkg/biology/fastp/fastp_v0.20.0/fastp -w 16 -i $var1/$var2\.read1.fastq \
-	-I $var1/$var2\.read2.fastq \
-	-o $var1/dealed/$var2\.read1.fq.gz \
-	-O $var1/dealed/$var2\.read2.fq.gz
+	/pkg/biology/fastp/fastp_v0.20.0/fastp -w 16 -i $var1/${var2}.read1.fastq \
+	-I $var1/${var2}.read2.fastq \
+	-o $var1/dealed/${var2}.read1.fq.gz \
+	-O $var1/dealed/${var2}.read2.fq.gz
 
-elif [ -f "$fileroad/$filename.read1.fastq.gz" ];
+elif [ -f "$var1/${var2}.read1.fastq.gz" ];
 then      
-	/pkg/biology/fastp/fastp_v0.20.0/fastp -w 16 -i $var1/$var2\.read1.fastq.gz \
-	-I $var1/$var2\.read2.fastq.gz \
-	-o $var1/dealed/$var2\.read1.fq.gz \
-	-O $var1/dealed/$var2\.read2.fq.gz
+	/pkg/biology/fastp/fastp_v0.20.0/fastp -w 16 -i $var1/${var2}.read1.fastq.gz \
+	-I $var1/${var2}.read2.fastq.gz \
+	-o $var1/dealed/${var2}.read1.fq.gz \
+	-O $var1/dealed/${var2}.read2.fq.gz
 fi
