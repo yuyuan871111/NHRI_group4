b638df9b4b334a22b5a35b3ea33b215d500a1be7
asd2213857 <47940934+asd2213857@users.noreply.github.com>
2020-10-26 15:34:08 +0800

    Add files via upload

--- autoscript/variantcalling.sh
+++ autoscript/variantcalling.sh
@@ -5,35 +5,36 @@
 #PBS -q ngscourse
 #PBS -o variantcalling.std
 #PBS -e variantcalling.err
-strelka=/pkg/biology/Strelka/Strelka_v2.9.9/bin/configureStrelkaGermlineWorkflow.py
+
+strelka=/pkg/biology/Strelka/Strelka_v2.9.9/bin/configureStrelkaSomaticWorkflow.py
 manta=/pkg/biology/Manta/Manta_v1.6.0/bin/configManta.py
-freec=/pkg/biology/FREEC/FREEC_v11.5/freec
 fileroad=$var1
-filename=$var2
+tumorname=$var2
+normalname=$var3
 refch38=/pkg/biology/reference/Homo_sapiens/NCBI/GRCh38/Sequence/WholeGenomeFasta/genome.fa
 
 mkdir $fileroad/dealed/manta
 mkdir $fileroad/dealed/strelka
-mkdir $fileroad/dealed/freec
-cd $fileroad/dealed/manta
-$manta --bam $fileroad/dealed/$filename\_sorted_mdp.bam \
---referenceFasta $refch38
 
-MantaWorkflow/runWorkflow.py --mode=local --jobs=16
+#run manta
+cd $fileroad/dealed/manta
+$manta --tumorBam $fileroad/dealed/$tumorname\_sorted_mdp.bam \
+--referenceFasta $refch38 \
+--exome \
+--runDir MantaSomaticAnalysis
+
+MantaSomaticAnalysis/runWorkflow.py --mode=local --jobs=16
 
 ## run strelka
 cd $fileroad/dealed/strelka
 $strelka \
---bam $fileroad/dealed/$filename\_sorted_mdp.bam \
---referenceFasta $refch38
+--normalBam $fileroad/dealed/$normalname\_sorted_mdp.bam \
+--tumorBam $fileroad/dealed/$tumorname\_sorted_mdp.bam \
+--referenceFasta $refch38 \
+--indelCandidates \
+$fileroad/dealed/manta/MantaSomaticAnalysis/results/variants/candidateSmallIndels.vcf.gz \
+--exome \
+--runDir StrelkaSomaticAnalysis
 
-StrelkaGermlineWorkflow/runWorkflow.py --mode=local --jobs=16
-
-## run freec
-cd $fileroad/dealed/freec
-$freec -conf $fileroad/dealed/freec/freec_config.txt
-
-mkdir $fileroad\/dealed/document
-mv *.std $fileroad\/dealed/document/
-mv *.err $fileroad\/dealed/document/
+StrelkaSomaticAnalysis/runWorkflow.py --mode=local --jobs=16
 
